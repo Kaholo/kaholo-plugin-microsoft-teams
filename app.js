fb93855353d60b29c6d2bdc7a7883b84eb6e3f45
xkcm <xkcm16@gmail.com>
2023-06-23 19:11:58 +0200

    Add linting

--- app.js
+++ app.js
@@ -1,28 +1,32 @@
-const request = require('request');
+const request = require("request");
 
-function sendIncomingWebhook(action,settings){
-    return new Promise((resolve,reject)=>{
-        
-        if(typeof action.params.message == 'string'){
-            action.params.message = {text: action.params.message}
-        }
+function sendIncomingWebhook(action) {
+  return new Promise((resolve, reject) => {
+    if (typeof action.params.message === "string") {
+      // eslint-disable-next-line no-param-reassign
+      action.params.message = { text: action.params.message };
+    }
 
-        const requestOptions = {
-            url: action.params.webhookUrl,
-            method: "post",
-            json : true,
-            body: action.params.message
-        }
-        
-        request(requestOptions, function (error, response, body) {
-            if (error) return reject(error);
-            if(response.statusCode !== 200) return reject(response);
-            
-            return resolve(body)
-        });
-    })
+    const requestOptions = {
+      url: action.params.webhookUrl,
+      method: "post",
+      json: true,
+      body: action.params.message,
+    };
+
+    request(requestOptions, (error, response, body) => {
+      if (error) {
+        return reject(error);
+      }
+      if (response.statusCode !== 200) {
+        return reject(response);
+      }
+
+      return resolve(body);
+    });
+  });
 }
 
 module.exports = {
-    sendIncomingWebhook: sendIncomingWebhook
-}
+  sendIncomingWebhook,
+};
